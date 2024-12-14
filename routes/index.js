39ae4636c82a6dc60137977655c9c3bfa428cad3
SanviSoni <sanvisoni8998@gmail.com>
2024-12-15 00:40:34 +0530

    Co-authored-by: Ujjwal  <U-jjwal@users.noreply.github.com> Co-authored-by: Aditya Shrivastav <Aditya580@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/login", (req, res, next) => {
-  res.render("login");
+router.get("/register", (req, res, next) => {
+  res.render("register");
 });
 
 module.exports = router;
