7606909ba2b752defdd3f150323fd5fd692f96b4
SanviSoni <sanvisoni8998@gmail.com>
2024-12-15 22:41:06 +0530

    Email feature update 2

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const User = require("../models/userSchema");
 const Letter = require("../models/letter");
 const {isLoggedIn} = require("../middleware/aunth")
 
+
+
+
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
@@ -21,21 +25,21 @@ router.get("/forget", (req, res, next) => {
   res.render("forget");
 });
 
-router.get("/profile",isLoggedIn ,async(req, res, next) => {
+router.get("/profile" ,async(req, res, next) => {
 
   let user = await User.findById(req.user?._id).populate("letter")
 
   console.log(user)
 
 
-  res.render("profile" ,{user:req.user});
+  res.render("profile" ,{user});
 });
 
 router.get("/letter", isLoggedIn,(req, res, next) => {
 
 
 
-  res.render("letter" , {user:req.user});
+  res.render("letter" );
 });
 
 
